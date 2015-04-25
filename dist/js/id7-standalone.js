312ab0ebd09b91aa6ee191be77cf3559afce4d90
Mat Mannion <mat.mannion@gmail.com>
2015-04-25 11:39:43 +0100

    publish: Merge branch 'master' into ID-105
    generated from commit 6f39557c5a330011c7cb9b8a084601d252ba9a41

--- dist/js/id7-standalone.js
+++ dist/js/id7-standalone.js
@@ -52,6 +52,10 @@
       this.$trigger = o.trigger;
       this.options = o;
 
+      if (typeof this.options.logoutlink === 'undefined') {
+        this.options.logoutlink = this.$trigger.attr('href');
+      }
+
       this.wireEventHandlers();
     }
 
@@ -59,19 +63,22 @@
       wireEventHandlers: function wireEventHandlers() {
         var $trigger = this.$trigger;
 
-        $trigger.on('click', function (e) {
-          e.preventDefault();
-          e.stopPropagation();
-          return false;
-        }).popover({
-          container: this.options.container,
-          content: Config.Templates.Popover(this.options),
-          template: this.options.template,
-          html: true,
-          placement: 'bottom',
-          title: 'Account information',
-          trigger: 'click'
-        });
+        $trigger
+          .on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+          })
+          .popover({
+            container: this.options.container,
+            content: Config.Templates.Popover(this.options),
+            template: this.options.template,
+            html: true,
+            placement: 'bottom',
+            title: 'Account information',
+            trigger: 'click'
+          })
+          .html(this.options.name + '<span class="caret"></span>');
 
         // Click away to dismiss
         $('html').on('click.popoverDismiss', function (e) {
@@ -254,11 +261,6 @@
           var offsetTop = $('.id7-header-text').offset().top;
           var headerTextHeight = $('.id7-header-text').height();
 
-          // FIXME magic number for xs - this comes into play where we have a parent site header
-          if (headerTextHeight > 52) {
-            offsetTop += (headerTextHeight - 52);
-          }
-
           $h1.affix({
             offset: {
               top: offsetTop
@@ -276,11 +278,6 @@
           offsetTop = $h1.offset().top;
 
           var headerTextHeight = $('.id7-header-text').height();
-
-          // FIXME magic number for xs - this comes into play where we have a parent site header
-          if (headerTextHeight > 52) {
-            offsetTop += (headerTextHeight - 52);
-          }
         } else {
           offsetTop = $nav.offset().top;
         }
